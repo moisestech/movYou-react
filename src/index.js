ad730899e82467953a72ceba1851b5b0d78c69ab
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-01 08:47:43 -0400

    app structure css html, index adds app with ReactDOM

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+
+// Component
+// State
+// Lifecycle
+// UI
+
+function App () {
+  return (
+    <h1>Welcome to MovYou!</h1>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
