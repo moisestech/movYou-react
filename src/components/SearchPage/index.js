155ddaa572ceeec81fcdc2558595d85eeba01ef7
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-03 04:44:48 -0400

    functional component ChannelRow, VideoRow List

---
+++ src/components/SearchPage/index.js
@@ -0,0 +1,16 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+export default function SearchPage () {
+  return (
+    <div className='search-page'>
+      SearchPage
+      {/* Channel Row */}
+      {/* Video Row List */}
+    </div>
+  )
+}
+
+SearchPage.propTypes = {
+
+}
