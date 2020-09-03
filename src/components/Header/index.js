9229e43855c3e3898a158616a13e354c4dfed94f
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-03 04:42:52 -0400

    function component HeaderLeft, HeaderLogo, HeaderInput, HeaderIcons

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+export default function Header () {
+  return (
+    <div className='header'>
+      Header
+      {/* HeaderLeft */}
+      {/* HeaderLogo*/}
+      {/* HeaderInput */}
+      {/* HeaderIcons */}
+    </div>
+  )
+}
+
+Header.propTypes = {
+
+}
