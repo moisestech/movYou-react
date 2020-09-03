672f0baef2687eb5b4baaa86b550b394f82ff700
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-03 04:44:04 -0400

    functional component videos list

---
+++ src/components/RecommendedVideos/index.js
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+
+export default function RecommendedVideos () {
+  return (
+    <div className='recommended-videos'>
+      <h2>Recommended Videos</h2>
+      {/* Recommended Videos List */}
+    </div>
+  )
+}
+
+RecommendedVideos.propTypes = {
+
+}
