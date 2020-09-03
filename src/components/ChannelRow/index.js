1aa1912dd57dc789c0b826ffd3f903fa0eee6f98
Moises Sanabria <sesmoi.data@gmail.com>
2020-09-03 04:55:49 -0400

    function component, image, channel, subs, noOfVideos, description, verified prop-types

---
+++ src/components/ChannelRow/index.js
@@ -0,0 +1,41 @@
+import * as React from 'react'
+import PropTypes from 'prop-types'
+import { Avatar } from '@material-ui/core'
+import VerifiedIcon from '@material-ui/icons/CheckCircleOutlineOutlined'
+
+export default function ChannelRow({
+  image,
+  channel,
+  subs,
+  noOfVideos,
+  description,
+  verified
+}) {
+  return(
+    <div className='channel-row'>
+      <Avatar
+        className='channel-row-logo'
+        alt={channel}
+        src={image} />
+      <div className='channel-row-text'>
+        <h4>
+          {channel} {verified && <VerifiedIcon />}
+        </h4>
+        <p>
+          {subs} subscribers - {noOfVideos} videos
+        </p>
+        <p>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+ChannelRow.propTypes = {
+  image: PropTypes.string.isRequired,
+  channel: PropTypes.string.isRequired,
+  subs: PropTypes.string.isRequired,
+  noOfVideos: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  verified: PropTypes.string.isRequired
+}
+
