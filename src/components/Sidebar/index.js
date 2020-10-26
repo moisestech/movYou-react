b9a52a73b48c718262ba5a233770aa105f120301
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-26 09:16:23 -0400

    func comp importing SidebarRow w/ prop Icon from material UI icons and titles

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,14 +1,32 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
+import "./sidebar.css";
 
-export default function Sidebar () {
+// COMPONENTS
+import SidebarRow from "../SidebarRow";
+
+// ICONS
+import HomeIcon from "@material-ui/icons/Home";
+import WhatshotIcon from "@material-ui/icons/Whatshop";
+import SubscriptionsIcon from "@material-ui/icons/Subscriptions";
+import VideoLibraryIcon from "@material-ui/icons/VideoLibrary";
+import HistoryIcon from "@material-ui/icons/History";
+import OndemandVideoIcon from "@material-ui/icons/OndemandVideo";
+import WatchLaterIcon from "@material-ui/icons/WatchLater";
+import ThumbUpAltOutlinedIcon from "@material-ui/icons/ThumbUpAltOutlined";
+import ExpandMoreOutlinedIcon from "@material-ui/icons/ExpandMoreOutlined";
+
+export default function Sidebar() {
   return (
-    <div className='sidebar'>
-      Sidebar
+    <div className="sidebar">
+      <SidebarRow selected Icon={HomeIcon} title="Home" />
+      <SidebarRow Icon={WhatshotIcon} title="Trending" />
+      <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />
+      <hr />
+      <SidebarRow Icon={VideoLibraryIcon} title="Library" />
+      <SidebarRow Icon={HistoryIcon} title="History" />
+      <SidebarRow Icon={OndemandVideoIcon} title="Your Videos" />
+      <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+      <SidebarRow Icon={ThumbUpAltOutlinedIcon} title="Liked Videos" />
+      <SidebarRow Icon={ExpandMoreOutlinedIcon} title="Show more" />
     </div>
-  )
-}
-
-Sidebar.propTypes = {
-
+  );
 }
