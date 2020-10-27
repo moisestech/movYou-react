bd1f314627855d5645362c6aa9588d2908301264
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-27 06:46:13 -0400

    data with VideoCard components

--- src/components/RecommendedVideos/index.js
+++ src/components/RecommendedVideos/index.js
@@ -1,15 +1,60 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
+import VideoCard from "../VideoCard";
+import "./recommended-videos.css";
 
-export default function RecommendedVideos () {
+export default function RecommendedVideos() {
   return (
-    <div className='recommended-videos'>
-      <h2>Recommended Videos</h2>
-      {/* Recommended Videos List */}
+    <div className="recommended-videos">
+      <h2>Recommended</h2>
+      <div className="recommended-videos-videos">
+        <VideoCard
+          title="Konstanza"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Jbm7P8PO0ABnbo4P0G8hdwHaE2%26pid%3DApi&f=1"
+          channel="William Cruz"
+          image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Jbm7P8PO0ABnbo4P0G8hdwHaE2%26pid%3DApi&f=1"
+        />
+        <VideoCard
+          title="Konstanza"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Jbm7P8PO0ABnbo4P0G8hdwHaE2%26pid%3DApi&f=1"
+          channel="William Cruz"
+          image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Jbm7P8PO0ABnbo4P0G8hdwHaE2%26pid%3DApi&f=1"
+        />
+        <VideoCard
+          title="Konstanza"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Jbm7P8PO0ABnbo4P0G8hdwHaE2%26pid%3DApi&f=1"
+          channel="William Cruz"
+          image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Jbm7P8PO0ABnbo4P0G8hdwHaE2%26pid%3DApi&f=1"
+        />
+        <VideoCard
+          title="Konstanza"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Jbm7P8PO0ABnbo4P0G8hdwHaE2%26pid%3DApi&f=1"
+          channel="William Cruz"
+          image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Jbm7P8PO0ABnbo4P0G8hdwHaE2%26pid%3DApi&f=1"
+        />
+        <VideoCard
+          title="Konstanza"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Jbm7P8PO0ABnbo4P0G8hdwHaE2%26pid%3DApi&f=1"
+          channel="William Cruz"
+          image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Jbm7P8PO0ABnbo4P0G8hdwHaE2%26pid%3DApi&f=1"
+        />
+        <VideoCard
+          title="Konstanza"
+          views="2.3M Views"
+          timestamp="3 days ago"
+          channelImage="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Jbm7P8PO0ABnbo4P0G8hdwHaE2%26pid%3DApi&f=1"
+          channel="William Cruz"
+          image="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse2.mm.bing.net%2Fth%3Fid%3DOIP.Jbm7P8PO0ABnbo4P0G8hdwHaE2%26pid%3DApi&f=1"
+        />
+      </div>
     </div>
-  )
-}
-
-RecommendedVideos.propTypes = {
-
+  );
 }
