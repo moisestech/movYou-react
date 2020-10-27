30e72b14fc00ede26b3af1458da679c16f80b82e
Moises Sanabria <sesmoi.data@gmail.com>
2020-10-27 06:44:04 -0400

    useState inputSearch, Material Icons, Router Link Header-Logo, Header-Input, Header-Icons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,52 @@
-import * as React from 'react'
-import PropTypes from 'prop-types'
+import "./Header.css";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
-export default function Header () {
+// ICONS
+import MenuIcon from "@material-ui/icons/Menu";
+import SearchIcon from "@material-ui/icons/Search";
+import VideoCallIcon from "@material-ui/icons/VideoCall";
+import AppsIcon from "@material-ui/icons/Apps";
+import NotificationsIcon from "@material-ui/icons/Notifications";
+import Avatar from "@material-ui/core/Avatar";
+
+export default function Header() {
+  const [inputSearch, setInputSearch] = useState("");
+
   return (
-    <div className='header'>
-      Header
-      {/* HeaderLeft */}
-      {/* HeaderLogo*/}
-      {/* HeaderInput */}
-      {/* HeaderIcons */}
+    <div className="header">
+      <div className="header-left">
+        <MenuIcon />
+        <Link to="/">
+          <img
+            className="header-logo"
+            src="https://raw.githubusercontent.com/moisestech/movYou-react/master/public/mov-you-logo-landscape.png"
+            alt="YouTube Logo"
+          />
+        </Link>
+      </div>
+      <div className="header-input">
+        <input
+          onChange={(e) => setInputSearch(e.target.value)}
+          value={inputSearch}
+          placeholder="Search"
+          type="text"
+        />
+        <Link to={`/search/${inputSearch}`}>
+          <SearchIcon className="header-input-button" />
+        </Link>
+      </div>
+      <div className="header-icons">
+        <VideoCallIcon className="header-icon" />
+        <AppsIcon className="header-icon" />
+        <NotificationsIcon className="header-icon" />
+        <Avatar
+          alt="Remy Sharp"
+          src="https://avatars2.githubusercontent.com/u/24712956?s=400&u=b71527e605ae1b748fc2d4157a842e57e427ad44&v=4"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-Header.propTypes = {
-
-}
+Header.propTypes = {};
